ccafcf7c3b69aad965526ac7af2163a9a620cabb
Subrata Bhuin <subrata.cse095@bppimt.ac.in>
2022-12-24 12:40:52 +0530

    Made a Responsive News Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 const selectElement = (selector) => {
     const element = document.querySelector(selector);
     if(element) return element;
-    throw new Error('Something went wrong! Make sure that ${selector} exists is typed correctly.');  
+    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
 };
 
 //Nav styles on scroll
@@ -16,6 +16,7 @@ const scrollHeader = () =>{
 }
 
 window.addEventListener('scroll', scrollHeader);
+
 // Open menu & search pop-up
 const menuToggleIcon = selectElement('#menu-toggle-icon');
 const formOpenBtn = selectElement('#search-icon');
@@ -28,7 +29,6 @@ const toggleMenu = () =>{
     menuToggleIcon.classList.toggle('activated');
 }
 
-
 menuToggleIcon.addEventListener('click', toggleMenu);
 
 // Open/Close search form popup
@@ -38,6 +38,7 @@ formCloseBtn.addEventListener('click', () => searchContainer.classList.remove('a
 window.addEventListener('keyup', (event) => {
     if(event.key === 'Escape') searchContainer.classList.remove('activated');
 });
+
 // Switch theme/add to local storage
 const body = document.body;
 const themeToggleBtn = selectElement('#theme-toggle-btn');
@@ -61,3 +62,29 @@ themeToggleBtn.addEventListener('click', function () {
 });
 
 // Swiper
+const swiper = new Swiper(".swiper", {
+    // How many slides to show
+    slidesPerView: 1,
+    // How much space between slides
+    spaceBetween: 20,
+    // Make the next and previous buttons work
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // Make the pagination indicators work
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    //Responsive breakpoints for how many slides to show at that view
+    breakpoints: {
+        // 700px and up shoes 2 slides
+        700: {
+          slidesPerView: 2
+        },
+        // 1200px and up shoes 3 slides
+        1200: {
+            slidesPerView: 3
+        }
+    }   
+});
